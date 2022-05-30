da3099980917ee53f7bf627fb996a58d5790c4d6
BrookeAL0012 <bbcecil17@gmail.com>
2022-05-30 12:50:41 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodOverloadingExercise
+{
+    class Program
+    {
+    public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    public static string Add(int x, int y, bool isTrue)
+
+        {
+            var sum = x + y;
+            
+            if(isTrue == true && sum >1)
+            {
+                return $"{sum} dollars";
+            }
+
+            else if(isTrue == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (isTrue == true && sum <1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+    }
+}
